c78b5f555d41a76fc31446c75b735056a7ed6831
ShailyGH <shailykunal@.com>
2024-03-13 21:45:40 -0400

    Dice.java and DiceTest.java completed.

--- src/test/java/DiceTest.java
+++ src/test/java/DiceTest.java
@@ -1,2 +1,20 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class DiceTest {
+    @Test
+    public void diceRoll()
+    {
+
+        Dice dice = new Dice(5);
+
+        Boolean actual = (5 <= dice.diceRoll() && dice.diceRoll() <= 30);
+        Boolean expected = (5 <= dice.diceRoll() && dice.diceRoll() <= 30);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
