63524a6f573d9d8964abf520bf33fae12f4a2aa1
ShailyGH <shailykunal@.com>
2024-03-13 21:40:57 -0400

    Bins.java and BinsTest.java completed.

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,21 @@
+import java.util.Random;
+
 public class Dice {
+        public int numberOfDice;
 
-
+    public Dice(int numberOfDice) {
+        this.numberOfDice = numberOfDice;
+    }
+    public Integer diceRoll()
+    {
+        int sumOfDice = 0;
+        for (int i = 0; i < numberOfDice; i++) {
+            sumOfDice += (int) Math.floor(Math.random() * 6) + 1;
+        }
+        return sumOfDice;
+    }
+    public int getNumberOfDice()
+    {
+        return numberOfDice;
+    }
 }
---
+++ src/test/java/BinsTest.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import static org.junit.Assert.*;
+
+public class BinsTest {
+    @Test
+    public void createStorageBinTest()
+    {
+        Dice dice = new Dice(4);
+        Bins bins = new Bins(dice);
+
+        bins.createStorageBin();
+        int actual = bins.countTotals.lastKey();
+        int expected = 24;
+
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void storeCountInBinTest()
+    {
+        Dice dice = new Dice(3);
+        Bins bins = new Bins(dice);
+        bins.storeCountInBin(dice, 50);
+        int actual = 0;
+        for (int i = 3; i < 18; i++)
+        {
+            actual += bins.countTotals.get(i);
+        }
+        int expected = 50;
+
+        Assert.assertEquals(expected, actual);
+    }
+}
